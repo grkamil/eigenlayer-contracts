af6d963049cdcdb3735a4e9cbf4ecead09967132
Bowen Li <bowenli86@gmail.com>
2025-01-10 14:44:46 -0800

    fix: update EigenPodManager and test

--- src/contracts/pods/EigenPodManager.sol
+++ src/contracts/pods/EigenPodManager.sol
@@ -160,7 +160,6 @@ contract EigenPodManager is
     function addShares(
         address staker,
         IStrategy strategy,
-        IERC20,
         uint256 shares
     ) external onlyDelegationManager returns (uint256, uint256) {
         require(strategy == beaconChainETHStrategy, InvalidStrategy());
